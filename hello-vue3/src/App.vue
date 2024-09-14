afffefcb4d76e77e7fcb5c5e977469db5a9fed42
tian-yuzhe1 <2506741037@qq.com>
2024-09-15 00:25:07 +0800

    :fire: 监视属性

--- hello-vue3/src/App.vue
+++ hello-vue3/src/App.vue
@@ -1,18 +1,20 @@
 <!-- html -->
 <template>
   <div class="app">
-    姓: <input type="text" v-model="firstName" /> <br />
-    名: <input type="text" v-model="lastName" /> <br />
-
-    <!-- 全名: <span> {{ fullName() }}</span> <br /> -->
     <!--
-      计算属性
-        1. 原理：底层借助 Object.defineproperty 方法提供的 getter 和 setter
-        2. 与 methods 实现啊相比, 内部有缓存机制
-        3. 计算属性修改, 必须写 set 响应, 且 set 要引起依赖项发生变化（fN lN 都要变）
+      监视属性
+        1. 当监视的属性变化时, 回调被调用
+      和计算属性的区别
+        1. 计算能做的, 监视都能做 但最好使用计算
+        2. 监视能做的, 计算不一定 比如 异步任务
+      两个重要的原则
+        1. 被 Vue 管理的函数写成普通函数
+        2，不被的(定时器的回调等)写成 lambda
       -->
-    全名: <span> {{ fullName }}</span> <br />
-    不可变全名: <span> {{ fixFullName }}</span>
+    <h2>今天天气很{{ info }}</h2>
+    <button @click="changeWhether">切换天气</button>
+    <h2>深层监视: {{ deep_ex.a }}</h2>
+    <button @click="changeA">上面的值加1</button>
   </div>
 </template>
 
@@ -21,32 +23,43 @@
 export default {
   data() {
     return {
-      firstName: "T",
-      lastName: "YZ",
+      isHot: true,
+      deep_ex: {
+        a: 1,
+      },
     };
   },
-  // methods: {
-  //   fullName() {
-  //     return this.firstName + "-" + this.lastName;
-  //   },
-  // },
-  computed: {
-    fullName: {
-      // 当有人读取 get get 会被调用, 且返回值作为 fullName 的值
-      // get 当 初次读取 fullName 或 所依赖的数据发生变化 时被调用
-      get() {
-        return this.firstName + "-" + this.lastName;
-      },
-      // set 当 fullName 被修改时被调用
-      set(v) {
-        const val = v.split("-");
-        this.firstName = val[0];
-        this.lastName = val[1];
-      },
+  methods: {
+    changeWhether() {
+      this.isHot = !this.isHot;
     },
-    // 当确定只用 get 时
-    fixFullName() {
-      return this.firstName + "-" + this.lastName;
+    changeA() {
+      this.deep_ex.a++;
+    },
+  },
+  computed: {
+    info() {
+      return this.isHot ? "炎热" : "寒冷";
+    },
+  },
+  watch: {
+    // 不开 deep, immediate 等配置
+    // 简写
+    info(new_val, old_val) {
+      console.log(new_val, old_val);
+    },
+    // "deep_ex.a": {
+    //   handler(new_val, old_val) {
+    //     console.log(new_val, old_val);
+    //   },
+    // },
+
+    // 不开 deep 则除非整体改变才会回调
+    deep_ex: {
+      deep: true,
+      handler() {
+        console.log("deep_ex changed.");
+      },
     },
   },
 };
