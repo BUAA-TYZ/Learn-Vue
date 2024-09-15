c33582ca938928fe1d0186933b5880f907707c67
tian-yuzhe1 <2506741037@qq.com>
2024-09-15 22:02:27 +0800

    :fire: 绑定样式

--- hello-vue3/src/App.vue
+++ hello-vue3/src/App.vue
@@ -1,20 +1,15 @@
 <!-- html -->
 <template>
-  <div class="app">
-    <!--
-      监视属性
-        1. 当监视的属性变化时, 回调被调用
-      和计算属性的区别
-        1. 计算能做的, 监视都能做 但最好使用计算
-        2. 监视能做的, 计算不一定 比如 异步任务
-      两个重要的原则
-        1. 被 Vue 管理的函数写成普通函数
-        2，不被的(定时器的回调等)写成 lambda
-      -->
-    <h2>今天天气很{{ info }}</h2>
-    <button @click="changeWhether">切换天气</button>
-    <h2>深层监视: {{ deep_ex.a }}</h2>
-    <button @click="changeA">上面的值加1</button>
+  <!-- 
+    Vue 专门为 class 和 style 的 v-bind 用法提供了特殊的功能增强
+    除了字符串外，表达式的值也可以是对象或数组 
+    1. 绑定class样式
+      可以传递数组 对象 字符串
+      :class 也可以传递一个数组, 表明需要应用的 class
+    2. 绑定style样式
+  -->
+  <div class="app" :class="color">
+    <button @click="changeColor">点我改变方框颜色</button>
   </div>
 </template>
 
@@ -23,43 +18,15 @@
 export default {
   data() {
     return {
-      isHot: true,
-      deep_ex: {
-        a: 1,
-      },
+      color: "border-blue",
     };
   },
   methods: {
-    changeWhether() {
-      this.isHot = !this.isHot;
-    },
-    changeA() {
-      this.deep_ex.a++;
-    },
-  },
-  computed: {
-    info() {
-      return this.isHot ? "炎热" : "寒冷";
-    },
-  },
-  watch: {
-    // 不开 deep, immediate 等配置
-    // 简写
-    info(new_val, old_val) {
-      console.log(new_val, old_val);
-    },
-    // "deep_ex.a": {
-    //   handler(new_val, old_val) {
-    //     console.log(new_val, old_val);
-    //   },
-    // },
-
-    // 不开 deep 则除非整体改变才会回调
-    deep_ex: {
-      deep: true,
-      handler() {
-        console.log("deep_ex changed.");
-      },
+    changeColor() {
+      const shapeSet = ["border-blue", "border-red", "border-green"];
+      const i = Math.floor(Math.random() * 3);
+      console.log(shapeSet[i]);
+      this.color = shapeSet[i];
     },
   },
 };
@@ -68,8 +35,19 @@ export default {
 <!-- css -->
 <style>
 .app {
-  background-color: aliceblue;
-  border-radius: 10px;
   padding: 20px;
+  border-radius: 20px;
+}
+
+.border-blue {
+  background-color: aliceblue;
+}
+
+.border-red {
+  background-color: red;
+}
+
+.border-green {
+  background-color: greenyellow;
 }
 </style>
